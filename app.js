1d4ad94cd6b10279210a6039b737941d8f216328
Shanna Ginos <Shanna@Dustins-MBP.directcom.com>
2021-06-14 15:05:06 -0600

    added edit functionality and delete functionality

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
+const { timeStamp } = require('console');
 const express = require('express');
 const path = require('path');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 
-let randomId = Math.ceil(Math.random() * 100); 
-
-var usersObj = {users:[
+let users = [
     {
-        userId: randomId,
+        userId: uuidv4(),
         username: 'dginos',
         name: 'Dustin',
         email: 'dustin@email.com',
         age: '29'
     },
     {
-        userId: randomId,
+        userId: uuidv4(),
         username: 'diego3',
         name: 'Diego',
         email: 'diego@email.com',
         age: '7'
     }
-]};
+];
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('views'));
@@ -31,22 +31,66 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/users/create', (req, res) => {
-    const user = {userId: randomId, username: req.body.username, name: req.body.name, email: req.body.email, age: req.body.age}
-    usersObj.users.push(user);
-    console.log(req.body.username);
-    res.redirect('/users/table');
+app.post('/create', (req, res) => {
+    const user = {userId: uuidv4(), username: req.body.username, name: req.body.name, email: req.body.email, age: req.body.age}
+    users.push(user);
+    res.redirect('/table');
 });
 
-app.get('/users/table', (req, res) => {
+app.get('/table', (req, res) => {
     res.render('users', {
-        users: usersObj.users
+        users: users
     });
 });
 
-app.get('users/edit/:userId', (req, res) => {
+app.get('/edit/:userId', (req, res) => {
     let userId = req.params.userId;
+    let userInfo = getUsers(userId);
+    res.render('editIndex', {
+        usernameField: userInfo[0],
+        nameField: userInfo[1],
+        emailField: userInfo[2],
+        ageField: userInfo[3],
+        userId: userId
+    });
 });
+
+app.post('/edit/:userId', (req, res) => {
+    let userId = req.params.userId;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].userId === userId) {
+            users[i].username = req.body.username;
+            users[i].name = req.body.name;
+            users[i].email = req.body.email;
+            users[i].age = req.body.age;
+        }
+    }
+    res.redirect('/table');
+});
+
+app.post('/table', (req, res) => {
+    let userId = req.body.userId;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].userId === userId) {
+            users.splice(i, 1);
+        }
+    }
+    res.redirect('/table');
+});
+
+function test()
+{
+    console.log("In test")
+}
+
+function getUsers(id) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].userId === id) {
+            let currentUser = [users[i].username, users[i].name, users[i].email, users[i].age]
+            return currentUser;
+        }
+    }
+}
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
